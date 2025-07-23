e78863469a1f2cca18118801fc7ccc29031b601d
Sebas Saenz <mssebasms@gmail.com>
2025-07-22 19:24:12 -0600

    Usuarios_roles añadido

---
+++ src/main/java/com/mycompany/proyectosegundocuatri/Usuarios.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectosegundocuatri;
+
+/**
+ *
+ * @author willu
+ */
+public class Usuarios {
+    private String nombre_rol;
+   int rol;
+   
+   public Usuarios(String nombre_rol, int rol) {
+       this.nombre_rol = nombre_rol;
+       this.rol = rol;
+   }
+   public String getNombre_rol() {
+       return nombre_rol;
+   }
+   public int getRol() {
+       return rol;
+ 
+   } 
+}
+    
+
